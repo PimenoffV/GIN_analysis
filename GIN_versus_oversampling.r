11fbbce7cebd0058e5e10ba254b3085fb81c3a04
VPimenoff <56830098+PimenoffV@users.noreply.github.com>
2021-10-21 01:48:10 +0200

    Update GIN_versus_oversampling.r

--- GIN_versus_oversampling.r
+++ GIN_versus_oversampling.r
@@ -1,4 +1,8 @@
-
+rm(list = ls())
+#install.packages(c("lemon", "patchwork", "scoring"))
+#install.packages("lemon")
+#install.packages("patchwork")
+#install.packages("scoring")
 library(lemon)
 library(patchwork)
 library(scoring)
@@ -154,7 +158,7 @@ library(scoring)
 library(bimba)
 
 
-dir.in<-"/PATH/"
+dir.in<-"PATH to the FOLDER"
 file.in<-"hpvsimTP.txt"
 fit.in.p<-paste(dir.in,file.in,sep="")
 hpv.sim<-as.data.frame(read.table(fit.in.p,header=T))
@@ -326,3 +330,148 @@ mat.Chev.vec.R[i.cond,]<-rank(mat.Chev.vec[i.cond,])
 
 }
 
+
+##########################################################################################
+################## FIGURE
+#########################################################################################
+
+table10 <- c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf")
+table20 <- c("#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5","#9c9ede", "#7375b5", "#4a5584", "#cedb9c", "#b5cf6b", "#8ca252","#7b4173")
+manyEyes <- c("#9c9ede", "#7375b5", "#4a5584", "#cedb9c", "#b5cf6b", "#8ca252", "#637939", "#e7cb94", "#e7ba52", "#bd9e39", "#8c6d31", "#e7969c", "#d6616b", "#ad494a", "#843c39", "#de9ed6", "#ce6dbd", "#a55194", "#7b4173")
+table4 <- c("wheat1", "tan1","salmon1","orangered1")
+
+dir.in<-"PATH to the FOLDER"
+#install.packages("ggplot2")
+library(ggplot2)
+ 
+# create a dataset
+# c("GIN","BDS1","BDS2","SM","ADA","MW","ROS","SLS")
+method <- c(rep("1.GIN" , 3) , rep("3.BDS1" , 3) , rep("4.BDS2" , 3) , rep("6.SM" , 3), rep("2.ADASYN" , 3), rep("8.MW" , 3), rep("5.ROS" , 3), rep("7.SLS" , 3))
+Scheme <- rep(c("SC1(N=050)" , "SC2(N=100)" , "SC3(N=250)") , 8)
+distance <- c(mat.Dist.vec.R$GM, mat.Dist.vec.R$BDS1, mat.Dist.vec.R$BDS2, mat.Dist.vec.R$SM, mat.Dist.vec.R$ADA, mat.Dist.vec.R$MW, mat.Dist.vec.R$ROS, mat.Dist.vec.R$SLS)
+Euclidean <- c(mat.Dist.vec.R$GM, mat.Dist.vec.R$BDS1, mat.Dist.vec.R$BDS2, mat.Dist.vec.R$SM, mat.Dist.vec.R$ADA, mat.Dist.vec.R$MW, mat.Dist.vec.R$ROS, mat.Dist.vec.R$SLS)
+measure <- c("Euclidean")
+data1 <- data.frame(method,Scheme,distance,measure)
+ 
+# Grouped
+
+dist.euc<-ggplot(data1, aes(fill=Scheme, y=distance, x=method)) + 
+    geom_bar(position="dodge", stat="identity")+ylab("Ranking Euclidean") + coord_flip() +
+  scale_fill_manual(values = manyEyes) +
+  facet_grid(.~Scheme)
+dist.euc
+library(ggplot2)
+ 
+# create a dataset
+# c("GM","BDS1","BDS2","SM","ADA","MW","ROS","SLS")
+method <- c(rep("1.GIN" , 3) , rep("3.BDS1" , 3) , rep("4.BDS2" , 3) , rep("5.SM" , 3), rep("2.ADASYN" , 3), rep("6.MW" , 3), rep("6.ROS" , 3), rep("8.SLS" , 3))
+Scheme <- rep(c("SC1(N=050)" , "SC2(N=100)" , "SC3(N=250)") , 8)
+ranking <- c(mat.Chev.vec.R$GM, mat.Chev.vec.R$BDS1, mat.Chev.vec.R$BDS2, mat.Chev.vec.R$SM, mat.Chev.vec.R$ADA, mat.Chev.vec.R$MW, mat.Chev.vec.R$ROS, mat.Chev.vec.R$SLS)
+Chebyshev <- c(mat.Chev.vec.R$GM, mat.Chev.vec.R$BDS1, mat.Chev.vec.R$BDS2, mat.Chev.vec.R$SM, mat.Chev.vec.R$ADA, mat.Chev.vec.R$MW, mat.Chev.vec.R$ROS, mat.Chev.vec.R$SLS)
+measure <- c("Chebyshev")
+data2 <- data.frame(method,Scheme,distance, measure)
+ 
+# Grouped
+dist.cheb<-ggplot(data2, aes(fill=Scheme, y=ranking, x=method)) + 
+    geom_bar(position="dodge", stat="identity")+ylab("Ranking Chebyshev")  + coord_flip() +
+  scale_fill_manual(values = manyEyes) +
+    facet_wrap(vars(Scheme))
+dist.cheb
+
+# Both separate together
+#############################
+data <- data.frame(method,Scheme,Euclidean, Chebyshev)
+str(data)
+distance <- c("Euclidean","Chebyshev")
+
+pdf(paste(dir.in,"Figure_4AB_individual.pdf", sep = ""), width = 6, height = 6)
+for(v in distance){
+  d=data[,c("method","Scheme",v)]
+  p <- ggplot(d, aes(fill=Scheme, y=get(v), x=method)) + 
+    geom_bar(position="dodge", width = 0.7, stat="identity") +
+    ylab(v) + scale_fill_manual(values = manyEyes) +
+  facet_wrap(vars(Scheme)) + coord_flip() 
+  print(p)
+  }
+dev.off()
+
+# Both plotted parallel
+#############################
+#install.packages("gridExtra")
+#install.packages("patchwork")
+#install.packages("arrangeGrob")
+library("ggplot2")
+library("gridExtra")
+library(grid)
+library(patchwork)
+p1 <- ggplot(data1, aes(fill=Scheme, y=distance, x=method)) + 
+  geom_bar(position="dodge", width = 0.7, stat="identity", colour="black")+ylab("Ranking Euclidean") + coord_flip() +
+  scale_fill_manual(values = manyEyes) + labs(tag = "A") +
+  theme(text = element_text(size=14))
+  #facet_grid(.~Scheme)
+
+p2 <- ggplot(data2, aes(fill=Scheme, y=ranking, x=method)) + 
+  geom_bar(position="dodge", width = 0.7, stat="identity", colour="black")+ylab("Ranking Chebyshev")  + coord_flip() +
+  scale_fill_manual(values = manyEyes) + labs(tag = "B") +
+  theme(text = element_text(size=14))
+  #facet_wrap(vars(Scheme))
+
+pdf(paste(dir.in,"Figure_4AB_patch.parallel.pdf", sep = ""), width = 12, height = 6)
+comb <- p1 + p2 & theme(legend.position = "right")
+comb + plot_layout(guides = "collect") 
+dev.off()
+
+postscript(paste(dir.in,"Figure_4AB_patch.parallel.ps", sep = ""), width = 12, height = 6)
+comb <- p1 + p2 & theme(legend.position = "right")
+comb + plot_layout(guides = "collect") 
+dev.off()
+
+# face_grip version
+#install.packages("lemon")
+
+library(lemon)
+pdf(paste(dir.in,"Figure_4AB_grid.parallel.pdf", sep = ""), width = 15, height = 8)
+#p1A <- arrangeGrob(p1, top = textGrob("A", x = unit(0, "npc")
+#                                               , y   = unit(1, "npc"), just=c("left","top"),
+#                                               gp=gpar(col="black", fontsize=18, fontfamily="Helvetica")))
+
+#p2B <- arrangeGrob(p2, top = textGrob("B", x = unit(0, "npc")
+#                                               , y = unit(1, "npc"), just=c("left","top"),
+#                                               gp=gpar(col="black", fontsize=18, fontfamily="Helvetica")))
+grid.arrange(p1, p2, nrow = 1) +
+grid_arrange_shared_legend(p1, p2, position = "bottom")
+dev.off()
+
+# face_wrap version
+#############################
+data0 <- rbind(data1, data2)
+str(data0)
+data0 <- data.frame(data0,Df = rep(c("data1","data2"),
+                                     times=c(nrow(data1),nrow(data2))))
+
+p <-  ggplot(data0, aes(fill=Scheme, y=distance, x=method)) + 
+  geom_bar(position="dodge", stat="identity")+ylab(measure)  + coord_flip() +
+  scale_fill_manual(values = manyEyes) +
+  facet_wrap(~Df)
+  #facet_grid(measure ~.)
+p
+
+install.packages("tidyverse")
+install.packages("dplyr")
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+
+p <- ggplot(mpg, aes(displ, hwy)) + geom_point()
+# Use vars() to supply faceting variables:
+p + facet_wrap(vars(class))
+# Control the number of rows and columns with nrow and ncol
+p + facet_wrap(vars(class), nrow = 4)
+# You can facet by multiple variables
+ggplot(mpg, aes(displ, hwy)) +
+  geom_point() +
+  facet_wrap(vars(cyl, drv))
+
+
+
+save.image("Sim.500.Samples.RData")
